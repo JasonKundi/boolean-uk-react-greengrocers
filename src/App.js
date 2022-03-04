39fcb64f4afef27278f7ad174d3283fa0aadd005
Jason Kundi <94011909+JasonKundi@users.noreply.github.com>
2022-03-04 16:52:55 +0000

    push

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './styles/reset.css'
 import './styles/index.css'
+import { useState } from 'react'
 
 import initialStoreItems from './store-items'
 
@@ -14,25 +15,91 @@ Here's what a store item should look like
 What should a cart item look like? 🤔
 */
 
-console.log(initialStoreItems)
-
 export default function App() {
-  // Setup state here...
+  const [storeItem, setStoreItem] = useState(initialStoreItems)
+  const [cartItem, setCartItem] = useState([])
+
+  const renderStoreItem = initialStoreItems => {
+    return initialStoreItems.map(storeItem => {
+      return (
+        <li>
+          <div className="store--item-icon">
+            <img
+              src={`/assets/icons/${storeItem.id}.svg`}
+              alt={storeItem.name}
+            />
+          </div>
+          <button onClick={() => addItemToCart(storeItem)} className="button">
+            Add to cart
+          </button>
+        </li>
+      )
+    })
+  }
+
+  const addItemToCart = storeItem => {
+    cartItem.find(cartItem => cartItem.id === storeItem.id)
+    setCartItem([...cartItem, { ...storeItem, quantity: 1 }])
+    console.log(cartItem)
+  }
+
+  const decrementQuantityInCart = storeItem => {
+    setCartItem(
+      cartItem.map(cartItem =>
+        cartItem.id === storeItem.id
+          ? { ...cartItem, quantity: storeItem.quantity-- }
+          : cartItem
+      )
+    )
+  }
+
+  const incrementQuantityInCart = storeItem => {
+    setCartItem(
+      cartItem.map(cartItem =>
+        cartItem.id === storeItem.id
+          ? { ...cartItem, quantity: storeItem.quantity++ }
+          : cartItem
+      )
+    )
+  }
+
+  const renderCartItem = () => {
+    return cartItem.map(item => {
+      return (
+        <li>
+          <img
+            className="cart--item-icon"
+            src={`/assets/icons/${item.id}.svg`}
+            alt={item.name}
+          />
+          <p>{item.name}</p>
+          <button onClick = {() => decrementQuantityInCart(storeItem)} className="quantity-btn remove-btn center">
+            -
+          </button>
+          <span className="quantity-text center">{item.quantity}</span>
+          <button
+            onClick={() => incrementQuantityInCart(storeItem)}
+            className="quantity-btn add-btn center"
+          >
+            +
+          </button>
+        </li>
+      )
+    })
+  }
 
   return (
     <>
       <header id="store">
         <h1>Greengrocers</h1>
         <ul className="item-list store--item-list">
-          {/* Wrtite some code here... */}
+          {renderStoreItem(initialStoreItems)}
         </ul>
       </header>
       <main id="cart">
         <h2>Your Cart</h2>
         <div className="cart--item-list-container">
-          <ul className="item-list cart--item-list">
-            {/* Wrtite some code here... */}
-          </ul>
+          <ul className="item-list cart--item-list">{renderCartItem()}</ul>
         </div>
         <div className="total-section">
           <div>
